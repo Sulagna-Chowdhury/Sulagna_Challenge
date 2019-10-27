8c63183f273025861c121f4862a111c3ca4404c5
lahas <lahasulagna@gmail.com>
2019-10-27 09:31:49 -0400

    Updated host name

--- challenge/challenge/settings.py
+++ challenge/challenge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b$)67e00850*-86y9akwbo#_qozc$-fjqq@)hjilcf9&(&hy6&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.19.219.144']
+ALLOWED_HOSTS = ['3.133.12.254']
 
 
 # Application definition
