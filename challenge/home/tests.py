b846acfd47af26327a1bc0de717a13f882d59eaa
lahas <lahasulagna@gmail.com>
2019-10-28 09:00:41 -0400

    Updated host name

--- challenge/home/tests.py
+++ challenge/home/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class IndexViewTests(TestCase):
+    def test_index_live(self):
+        """
+        We test if we are getting the correct reposnse when we visit the index view.
+        """
+        response = self.client.get(reverse('home:index'))
+        self.assertEqual(response.status_code, 200)
